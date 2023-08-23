3cbad697a980db4245c3ee8a19e149553bd2d3a1
alexpira <77795852+alexpira@users.noreply.github.com>
2023-08-23 11:26:08 +0200

    Removed comment

--- src/lib.rs
+++ src/lib.rs
@@ -10,21 +10,6 @@ mod levels;
 mod engine;
 mod view;
 
-/*
-#[wasm_bindgen]
-extern {
-	pub fn alert(s: &str);
-	pub fn r2j(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-	alert("Calling...");
-	r2j(&format!("Hell, {}!", name));
-	alert("Called");
-}
-*/
-
 #[wasm_bindgen(start)]
 fn start() {
 	let view = MenuView::new(target_elem(), MenuView::default_data());
